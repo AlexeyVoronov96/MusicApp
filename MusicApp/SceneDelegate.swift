5b8f806d5d09bc1d5c269a4a018ca9262044d50d
Alexey <alexey.voronov96@gmail.com>
2019-09-15 09:26:49 +0500

    adding gitignore file

--- MusicApp/SceneDelegate.swift
+++ MusicApp/SceneDelegate.swift
@@ -18,6 +18,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
+        
+        window = UIWindow()
+        window?.makeKeyAndVisible()
+        window?.rootViewController = TabBarController()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
---
+++ MusicApp/TabBarController.swift
@@ -0,0 +1,12 @@
+//
+//  TabBarController.swift
+//  MusicApp
+//
+//  Created by Алексей Воронов on 15.09.2019.
+//  Copyright © 2019 Алексей Воронов. All rights reserved.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+}
